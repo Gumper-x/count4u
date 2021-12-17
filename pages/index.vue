657d23855287965643519c0e59d19837950f1437
Andrey Berezhnyi <berezhnyi@it-enterprise.com>
2021-12-17 02:47:49 +0200

    feat: update

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,9 @@
 <template>
-  <div>
-    {{ lol }}
-  </div>
+  <div>lol</div>
 </template>
 
 <script>
-export default {
-  async asyncData({ $axios }) {
-    const data = await $axios.$get(
-      'https://jsonplaceholder.typicode.com/todos/1'
-    )
-    console.log(data)
-    return { lol: data }
-  },
-}
+  export default {
+    name: "IndexPage",
+  };
 </script>
